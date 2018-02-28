45536899780197f9b54c0f42702575b3d583f6f3
Tyler Helwig <tyler.helwig@mepush.com>
2018-02-28 15:05:38 -0500

    add gitsend function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,8 @@
 Import-Module posh-git
 Set-Location C:\git\
 Function ffdeploy {firebase deploy --only functions}
+Function gitsend([string]$commitmessage){
+	git add .
+	git commit -m $commitmessage
+	git push
+}
